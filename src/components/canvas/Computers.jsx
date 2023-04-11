a29f74259a9aa1d6905aa6d41faf1afc831cea37
KongLimheng <74581539+KongLimheng@users.noreply.github.com>
2023-04-11 15:40:47 +0700

    3d

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,20 @@ import { Canvas } from '@react-three/fiber';
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
-  return <div>Computers</div>;
+
+  console.log(computer);
+  return (
+    <mesh>
+      Computers
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
+};
+
+const ComputerCanvas = () => {
+  return <Canvas frameloop="demand"></Canvas>;
 };
 
 export default Computers;
